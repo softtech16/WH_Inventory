52f8d245365d7fbde91504f852673a1944bb12e0
John Krypton Yu <technology.softtech@gmail.com>
2016-06-30 02:07:46 +0800

    Added Login Validation with user table

--- WH_Inventory/Login.cs
+++ WH_Inventory/Login.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace WH_Inventory
 {
@@ -19,9 +20,37 @@ namespace WH_Inventory
 
         private void btnlogin_Click(object sender, EventArgs e)
         {
-            Menu menu = new Menu();
-            this.Hide();
-            menu.ShowDialog();
+            try
+            {
+                string loginConnection = "datasource=localhost;port=3306;username=root;password=090807";
+                MySqlConnection loginConn = new MySqlConnection(loginConnection);
+                MySqlCommand LoginCommand = new MySqlCommand("select * from wh_inventory.user where username='" + this.tbxuser.Text + "' and password='" + this.tbxpass.Text + "';", loginConn);
+                MySqlDataReader loginReader;
+                loginConn.Open();
+                loginReader = LoginCommand.ExecuteReader();
+                int count = 0;
+                while (loginReader.Read())
+                {
+                    count = count + 1;
+
+                }
+                if (count == 1)
+                {
+                    MessageBox.Show("Login Successfull. Welcome! ");
+                    Menu menu = new Menu();
+                    this.Hide();
+                    menu.ShowDialog();
+                }
+                else
+                    MessageBox.Show("Access Denied");
+                loginConn.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
         }
 
     }
